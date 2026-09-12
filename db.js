8b02e6bce54c78c5749dc6bd211cc88855be6432
m03234232-prog <m03234232@gmail.com>
2026-09-12 00:22:33 -0700

    Create db.js

---
+++ db.js
@@ -0,0 +1,11 @@
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.MONGODB_URI);
+
+async function connectToDatabase() {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    return client.db("giftshop");
+}
+
+module.exports = { connectToDatabase };
